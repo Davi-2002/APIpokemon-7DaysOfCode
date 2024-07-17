ea9607084697b5fca91fce6979ecce96ee3d37d4
Davi <@davialmeida120402@gmail.com>
2024-07-16 22:10:18 -0300

    criando um menu interativo

--- APIpokemon - 7DaysOfCode/Program.cs
+++ APIpokemon - 7DaysOfCode/Program.cs
@@ -1,41 +1,264 @@
 ﻿using RestSharp;
 using System.Text.Json;
 
-Console.Write("Escolha um desses pokémons para poder cuidar:" +
-  "\n\n************" +
-    "\n* umbreon  *" +
-    "\n* espeon   *" +
-    "\n* vaporeon *" +
-    "\n************" +
-    "\n\nagora digite o nome do pokémon que você decidiu ter: ");
+Exibir.Menu();
 
-string pokemon = Console.ReadLine()!;
-
-if (pokemon.Equals("umbreon") || pokemon.Equals("espeon") || pokemon.Equals("vaporeon"))
+public static class Exibir
 {
-    var client = new RestClient($"https://pokeapi.co/api/v2/pokemon/{pokemon}");
-    var request = new RestRequest("", Method.Get);
-    var response = client.Execute(request);
+    private static string? nome;
+    private static string? pokemon;
+    private static string[] pokemonsValidos = { "umbreon", "espeon", "vaporeon" };
 
-    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+    public static void Menu()
     {
-        var bichano = JsonSerializer.Deserialize<Mascote>(response.Content!);
-        bichano!.ExibirBichin();
+        Console.WriteLine(" ______   ______     __  __     ______     __    __     ______     __   __    \r\n/\\  == \\ /\\  __ \\   /\\ \\/ /    /\\  ___\\   /\\ \"-./  \\   /\\  __ \\   /\\ \"-.\\ \\   \r\n\\ \\  _-/ \\ \\ \\/\\ \\  \\ \\  _\"-.  \\ \\  __\\   \\ \\ \\-./\\ \\  \\ \\ \\/\\ \\  \\ \\ \\-.  \\  \r\n \\ \\_\\    \\ \\_____\\  \\ \\_\\ \\_\\  \\ \\_____\\  \\ \\_\\ \\ \\_\\  \\ \\_____\\  \\ \\_\\\\\"\\_\\ \r\n  \\/_/     \\/_____/   \\/_/\\/_/   \\/_____/   \\/_/  \\/_/   \\/_____/   \\/_/ \\/_/ \r\n                                                                              ");
+        Console.WriteLine("\nSeja bem vindo! qual é o seu nome?");
+        nome = Console.ReadLine()!;
+        
+        if (!string.IsNullOrEmpty(nome))
+        {
+            Console.WriteLine($"\nPrazer {nome}! espero que esteja bem ^^");
+            Thread.Sleep(2000);
 
-        // Console.WriteLine(response.Content);
-        // Console.WriteLine();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine($"o que gostaria de fazer agora {nome}?");
+                Console.WriteLine("1 - Adotar um Pokémon" +
+                                 "\n2 - Ver seus Pokémons" +
+                                 "\n3 - Sair");
+                try
+                {
+                    int escolha = int.Parse(Console.ReadLine()!);
+
+                    switch (escolha)
+                    {
+                        case 1:
+                            Adotar();
+                            break;
+
+                        case 2:
+                            VerMascotes();
+                            break;
+
+                        case 3:
+                            Console.Clear();
+                            Console.WriteLine("\naté mais! :)");
+                            return;
+
+                        default:
+                            Console.WriteLine("\nnão existe esta opção! tente digitar 1, 2 ou 3.");
+                            Thread.Sleep(3000);
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("\nacho que você se esqueceu de escolher uma opção! vamos tentar denovo...");
+                    Thread.Sleep(3000);
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("você não colocou seu nome :(");
+        }
+        
     }
-    else
+
+    private static void Adotar()
     {
-        Console.WriteLine(response.ErrorMessage);
-        Console.WriteLine();
+        Console.Clear();
+        Console.Write($"Escolha um desses pokémons {nome}:" +
+                 "\n\n************" +
+                   "\n* umbreon  *" +
+                   "\n* espeon   *" +
+                   "\n* vaporeon *" +
+                   "\n************" +
+                 "\n\ndigite o nome do pokémon: ");
+
+        pokemon = Console.ReadLine()!.ToLower();
+
+        if (pokemonsValidos.Contains(pokemon))
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine($"o que gostaria de fazer agora {nome}?");
+                Console.WriteLine($"1 - Saber mais sobre {pokemon}" +
+                                $"\n2 - Adotar {pokemon}" +
+                                 "\n3 - Voltar");
+                try
+                {
+                    int escolha = int.Parse(Console.ReadLine()!);
+
+                    switch (escolha)
+                    {
+                        case 1:
+                            SaberMais();
+                            break;
+
+                        case 2:
+                            AdoçãoPokemon();
+                            return;
+
+                        case 3:
+                            Console.Clear();
+                            return;
+
+                        default:
+                            Console.WriteLine("\nnão existe esta opção! tente digitar 1, 2 ou 3.");
+                            Thread.Sleep(3000);
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("\nacho que você se esqueceu de escolher uma opção! vamos tentar denovo...");
+                    Thread.Sleep(3000);
+                }
+            }
+ 
+        }
+        else
+        {
+            Console.WriteLine("\nvocê não pode escolher esse pokémon ou ele não existe");
+            Thread.Sleep(3000);
+        }
     }
 
-    Console.ReadKey();
-}
-else
-{
-    Console.WriteLine("\nvocê não pode escolher esse pokémon ou ele não existe\n");
+    private static void VerMascotes()
+    {
+        Console.Clear();
+        Console.WriteLine("ainda não implementado");
+        Thread.Sleep(2000);
+    }
+        
+    private static void SaberMais()
+    {
+        Console.Clear();
+        
+        var client = new RestClient($"https://pokeapi.co/api/v2/pokemon/{pokemon}");
+        var request = new RestRequest("", Method.Get);
+        var response = client.Execute(request);
+
+        if (response.StatusCode == System.Net.HttpStatusCode.OK)
+        {
+            var bichano = JsonSerializer.Deserialize<Mascote>(response.Content!);
+            bichano!.ExibirBichin();
+
+            // Console.WriteLine(response.Content);
+            // Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine(response.ErrorMessage);
+            Console.WriteLine();
+        }
+
+        Console.Write("\naperte qualquer tecla para voltar!");
+        Console.ReadKey();
+    }
+
+    private static void AdoçãoPokemon()
+    {
+        Console.Clear();
+        Console.WriteLine($"Boa {nome}! o {pokemon} agora é seu! cuide bem dele!");
+
+        switch(pokemon)
+        {
+            case "espeon":
+                Console.WriteLine(@"                                                
+        %                                       
+       +-                                       
+       =--+                                     
+       ##---#                                   
+       @##=--=*                                 
+        %##===---+                       +-:=   
+        ###=::::::-=-----=+#%  %*     =-::-=*   
+  #=-===++*-------=----------==*    +--+        
+   ****+*+---+======*####***#%     *-=+===-===  
+         +=-===-:*+=*#####%       *-====++====  
+          *====+====--=           -:=%          
+            #*+====+=-:-         #--            
+             #+=====*=-:=         --            
+             +=------* *#         =-%           
+             =--------=           =-#           
+             ----------=*         -:%           
+             -::---------=+      *-=            
+             %-:-==:--------=+   =-=            
+              =--=+--:::::-----*--+             
+              *--=-::::::--------*              
+              *--=-------------+=               
+              -=++==--------====*               
+             *+*#*+============+                
+                      %%%@                      
+                                                ");
+                break;
+            case "umbreon":
+                Console.WriteLine(@"                                                
+                         +++#                   
+                          ++--+--               
+                           #+-..-+#             
+                             --++++#            
+                               #++++#           
+                                 +++++#         
+     ++#                 ###  #-----...-#       
+      +---++       ++-...------------...+#      
+      #------..- +-----..----+++---.+-----#     
+       +-----. .--   ##+.-+   --------++++      
+        ++--.  .--++          +-----++++        
+         -.....----+#        #----++++          
+           #+++++++++------++--------+          
+               ##++++----------------+          
+                #+--------------------          
+                ++--------------------#         
+                +++++-....-+++++--..--          
+               #+++++..-..+ #++++..-.#          
+              +-+++++-..-#  ##+++-..+           
+              +--++-+++#    #+++++-+            
+              #--++-+#       +++#-++            
+               --++-+         #+++++            
+               ++++--+#         +++++#          
+                    +--+           ++#          
+                                                ");
+                break;
+            case "vaporeon":
+                Console.WriteLine(@"                                                  
+                    #                             
+                    ##                @%          
+                    %#%            #*=-+          
+                     ##%        %#+=:::+          
+                   #-=#=:::=+#%#*=::-=*           
+                 -:.-**#****=**=---::             
+     %****##%##%=.:++-+*+---+*-==-::*             
+      #-::..:-=+***-------=*#+=----:%             
+        *+-----====#@*+---=*+==----:=             
+           =:.-=======--+++====::-+               
+            #:..:-:====+***=++:::+*               
+             #:*:...-====++=--:-=*                
+                +-:.:.:-------:-+                 
+                ##+-:::-::--=+*                   
+               %#+-----=-=+++#                    
+          %###**+---------=++#                    
+      @%#*++=------------+++++       +====+#      
+      #*=-----------=----++++* %%%%=--------=#    
+    %**=------------==-=*####*++++*+=------+#     
+    %#+--+=-------=+*###+=====*% *----+##@        
+    %*+-----===+++#**====++*#    #------*         
+    *=**+*++*##*++=====++*+*%     *======%        
+    *---=====----====++++*+*       #+====+        
+     #+-------=====+# *+++=+%         *+==%       
+        #*++=++**#     *+##%            %#        
+                                                  ");
+                break;
+            default:
+                break;
+        }
+
+        Console.Write("\naperte qualquer tecla para voltar ao menu!");
+        Console.ReadKey();
+    }
 }
 
 public class Mascote
@@ -63,7 +286,7 @@ public class Mascote
 
 public class Ability
 {
-    public abilityName? ability {  get; set; }
+    public abilityName? ability { get; set; }
 
     public class abilityName
     {
